2ab7162cd5dd20267af4d677bf3e552274a8d5b0
Kenyi De la cruz <kenyi_dlcp_93@outlook.com>
2024-01-28 21:20:55 -0500

    first commit

---
+++ src/main/java/com/retrofit/product/rsproduct/RsProductApplication.java
@@ -0,0 +1,13 @@
+package com.retrofit.product.rsproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retrofit/product/rsproduct/RsProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retrofit.product.rsproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
